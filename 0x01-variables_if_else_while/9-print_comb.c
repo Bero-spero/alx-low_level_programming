573570f892ddd9c659b07ed273dad52a69baaeb6
Bero-spero <zoro234564@gmail.com>
2023-09-10 00:57:30 -0700

    999

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -prints all possible combinations of single-digit numbers
+ * Return: Always 0 (sucsses)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 58)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
