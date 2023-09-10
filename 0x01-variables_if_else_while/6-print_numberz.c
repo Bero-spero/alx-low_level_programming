157aaddf39879bb95635b630d1208d67d16fdc83
Bero-spero <zoro234564@gmail.com>
2023-09-10 00:24:39 -0700

    999

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - printball single digits number between 0 to 10
+ * fowlled by new line using putchar
+ * return 0 always
+*/
+int main(void)
+	}
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
