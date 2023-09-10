6fd8f44fb12f4333c0a8c1c0ef61f157aa626930
Bero-spero <zoro234564@gmail.com>
2023-09-09 17:01:30 -0700

    999

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,9 +5,9 @@
  *   *
  *    * Return: Always 0 (Success)
  */
-int main(void)
-{
-int i;
+	int main(void)
+	{
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
@@ -15,5 +15,5 @@ int i;
 	}
 				putchar('\n');
 	return (0);
-}
+	}
 
