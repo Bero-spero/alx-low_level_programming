3d095a2ba936f6477ac46820ddc7e8b02e692a3c
Bero-spero <zoro234564@gmail.com>
2023-09-13 11:29:22 -0700

    999

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,11 +10,8 @@ int main(void)
 
 		for (y = 0; y < 1024; y++)
 		{
-			if (y % 3 == 0)
-			{
-			if (y % 5 == 0)
+			if ((y % 3 == 0) || (y % 5 == 0))
 				sum = sum + y;
-			}
 		}
 		printf("%d\n", sum);
 		
