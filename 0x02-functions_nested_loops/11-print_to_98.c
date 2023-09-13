d6f0a7d8145164a4e59e146558a2185c008bdf6b
Bero-spero <zoro234564@gmail.com>
2023-09-13 02:50:31 -0700

    999

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include"main.h"
+
 /**
- * * jack_bauer - prints every minute of the day of Jack Bauer
+* jack_bauer - prints every minute of the day of Jack Bauer
 * starting from 00:00 to 23:59
 */
 void print_to_98(int n)
