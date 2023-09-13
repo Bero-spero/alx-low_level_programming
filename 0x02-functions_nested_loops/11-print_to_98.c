d19c01dc409a4b28b2e1ec372bc3538966859b22
Bero-spero <zoro234564@gmail.com>
2023-09-13 03:47:53 -0700

    999

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,11 +2,12 @@
 #include"main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
- * Description : function that prints all natural numbers from 0 to 98
-*/
-
+ * print_to_98 - Check Holberton
+ *   * @n: A input integer
+ *   * Description: function that prints all natural numbers
+ *   * from n to 98, followed by a new line.
+ *   * Return: Nothing
+ */
 void print_to_98(int n)
 {
 	if (n <= 98)
