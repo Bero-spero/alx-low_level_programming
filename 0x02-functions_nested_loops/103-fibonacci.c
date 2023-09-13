cb1d621dacfdc662a08a7585dd292b2f80d494af
Bero-spero <zoro234564@gmail.com>
2023-09-13 13:15:29 -0700

    999

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,25 +1,20 @@
 #include<stdio.h>
-/**
- *   *main - print first 50 fibonacci
- *     *
- *       *Return: 0 always.
+/**main - print first 50 fibonacci
+ *  *Return: 0 always.
 */
 int main(void)
 {
-	int c;
-	long int n1, n2, fn;
-
-	n1 = 1;
-	n2 = 2;
-	printf("%ld, %ld", n1, n2);
-
-	for (c = 0; c < 48; c++)
-	{
-		fn = n1 + n2;
-		printf(",%ld", fn);
-		n1 = n2;
-		n2 = fn;
+	    long int i, x = 1, y = 2, sum = 0, tSum = 0;
+	        for (i = 0;i < 49; i++)
+			{
+				if ((y % 2 == 0) && (y <= 4000000))
+		{
+		tSum = tSum + y;
+		{
+		sum = x + y;
+		x = y;
+		y = sum;
 	}
-	printf("\n");
-	return (0);
+	printf("%ld\n", tSum);
+	return(0);
 }
