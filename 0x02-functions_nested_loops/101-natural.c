ff999b42e01306ec850894b5995ccbbc8d635f6a
Bero-spero <zoro234564@gmail.com>
2023-09-13 11:45:12 -0700

    999

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
  /**
- * main - check for all multiples of 3 and 5 below 1024 .
- * Return: 0 always
+* main - Lists all the natural numbers below 1024 (excluded) that are multiples of 3 or 5.
+* Return: Always 0.
 */
 int main(void)
 {
