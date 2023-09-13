37388b8d17a8e21a6c205678dfb2862bd217b711
Bero-spero <zoro234564@gmail.com>
2023-09-13 04:04:02 -0700

    999

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,7 +5,9 @@
 */
 void print_times_table(int n)
 {
+
 	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -33,7 +35,7 @@ void print_times_table(int n)
 			_putchar((k / 10) + '0');
 			_putchar((k % 10) + '0');
 			}
-			       	else if (k >= 100)
+			else if (k >= 100)
 			{
 			_putchar(',');
 			_putchar(' ');
