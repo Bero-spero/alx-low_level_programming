2cf39e3cc428b905efc351c67cae65cc0de7cc32
Bero-spero <zoro234564@gmail.com>
2023-09-14 15:42:10 -0700

    999

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * * print_diagonal - Description: A C program that print
+ * * @n: input
+ * * Return: void
+*/
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j
+			;
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
