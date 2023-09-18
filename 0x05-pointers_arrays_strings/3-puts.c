a4e5e4aa8089c7b281a15c99e38d99273043575f
Bero-spero <zoro234564@gmail.com>
2023-09-18 12:55:37 -0700

    999

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * _puts-prints a string,followed by a new line
+ *@str:pointer for the string
+ *Return:Void
+ */
+void_puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+
+}
