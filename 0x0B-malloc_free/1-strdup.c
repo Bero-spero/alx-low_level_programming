4d2015835ad09ee043d3a49fd5034afa71ec9c26
Bero-spero <zoro234564@gmail.com>
2023-10-04 13:14:53 -0700

    999

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - allocated space in memory
+ * @str: pointer
+ * Return: pointert to
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i, j;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	ptr = malloc((sizeof(char) * i) + 1);
+	if (ptr == NULL)
+		return (NULL);
+	for (j = 0; j < i ; j++)
+		ptr[j] = str[j];
+	return (ptr);
+}
