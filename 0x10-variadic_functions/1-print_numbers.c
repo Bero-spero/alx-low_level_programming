f396fcec80d63c13aa6394ebffb5cdae67e4bf86
Bero-spero <zoro234564@gmail.com>
2023-10-12 12:57:42 -0700

    999

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include"variadic_functions.h"
+/**
+ *print_numbers-printsnumbersfollowedbyanewline
+ *@n:numberofparameter
+ *@separator:separatorbetweentwonumbers
+ *Return:Always0
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_listargs;
+	unsigned int i;
+
+	vastart(args, n);
+	if (separator == NULL)
+		separator = "";
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
